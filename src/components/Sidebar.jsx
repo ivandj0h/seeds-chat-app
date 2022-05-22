1e76c917b1314d48d25a34ada1ca219b6e93b961
ivandjoh <ivandi.djoh@gmail.com>
2022-05-23 04:01:35 +0800

    modified message form & update chat page

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,8 +1,21 @@
 import React from 'react'
+import { ListGroup } from 'react-bootstrap';
 
 function Sidebar() {
+    const group = ['Engineering', 'Marketing', 'Finance', 'Operations'];
     return (
-        <div>Sidebar</div>
+        <>
+            <h3>Groups Chats</h3>
+            <ListGroup>
+                {group.map((item, index) => {
+                    return (
+                        <ListGroup.Item key={index}>{item}</ListGroup.Item>
+                    )
+                })}
+            </ListGroup>
+            <h3>Users</h3>
+
+        </>
     )
 }
 
